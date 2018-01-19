256815558626b3ca3583d5d739af2baf13fa1b5e
Aaron Vinson <avinson.public@gmail.com>
2018-01-19 13:50:30 -0800

    dont change dirs in case we want to override later

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -7,9 +7,8 @@ source activate python2
 # only download workshop materials if default command has not been overridden
 if [ $1 = "start-notebook.sh" ]; then
   if [ ! -f ~/work/satellites-predictive.ipynb ]; then
-    cd ~/work && wget --progress=dot:giga -O - https://${CONTENT_URL} | gunzip -c | tar xf -
+    wget --progress=dot:giga -O - https://${CONTENT_URL} | gunzip -c | tar xf - -C ~/work
   fi
 fi
 
-cd ~
 exec "$@"
