220c877d7695795232a2b6cd5ce62f686fdd61a4
Aaron Vinson <avinson.public@gmail.com>
2018-02-22 13:28:37 -0800

    put conda uninstall first

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -9,11 +9,11 @@ if [ $1 = "start-notebook.sh" ]; then
   if [ -n "$DEVELOP_REPOS" ]; then
     echo "Enabling develop on: $DEVELOP_REPOS"
     for repo in $DEVELOP_REPOS; do
+      echo "  Uninstalling $repo conda package"
+      conda uninstall --quiet --yes $repo >/dev/null 2>&1
       echo "  Creating $repo.egg-link"
       echo -e "/home/$NB_USER/$repo/build/lib\n." > /opt/conda/envs/python2/lib/python2.7/site-packages/$repo.egg-link
       echo "/home/$NB_USER/$repo/build/lib"  >>  /opt/conda/envs/python2/lib/python2.7/site-packages/easy-install.pth
-      echo "  Uninstalling $repo conda package"
-      conda uninstall --quiet --yes $repo >/dev/null 2>&1
       echo "  Building $repo"
       bash -c "cd /home/$NB_USER/$repo && rm -rf build && python setup.py build" >/dev/null 2>&1
       if [ $(id -u) == 0 ] ; then
