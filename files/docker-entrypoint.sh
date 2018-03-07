b14650358f050de37ed44531d62fd6f73ed1efc0
Aaron Vinson <avinson.public@gmail.com>
2018-03-07 03:06:41 -0800

    ensure .local dir is owned by correct UID when sudo enabled

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -27,6 +27,7 @@ if [ $1 = "start-notebook.sh" ]; then
   echo "Trusting tutorial notebooks"
   if [ $(id -u) == 0 ] ; then
     sudo -u $NB_USER -E /opt/conda/envs/python2/bin/jupyter trust /home/$NB_USER/tutorials/*.ipynb >/dev/null
+    chown -R $NB_UID /home/$NB_USER/.local
   else
     /opt/conda/envs/python2/bin/jupyter trust /home/$NB_USER/tutorials/*.ipynb >/dev/null
   fi
