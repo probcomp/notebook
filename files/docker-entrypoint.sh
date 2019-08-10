4cd430fc8b241221f698680cf9d84ab06c914f81
Aaron Vinson <avinson@users.noreply.github.com>
2019-08-09 23:15:33 -0700

    Update docker-entrypoint.sh

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [ $1 = "start-notebook.sh" ]; then
       echo "/home/$NB_USER/$repo/build/lib"                  >> /opt/conda/envs/python2/lib/python2.7/site-packages/easy-install.pth
       echo "/home/$NB_USER/$repo/build/lib.linux-x86_64-2.7" >> /opt/conda/envs/python2/lib/python2.7/site-packages/easy-install.pth
       echo "  Building $repo"
-      bash -c "cd /home/$NB_USER/$repo && rm -rf build && python setup.py build" >/dev/null 2>&1
+      bash -c "cd /home/$NB_USER/$repo && rm -rf build && python2 setup.py build" >/dev/null 2>&1
       if [ $(id -u) == 0 ] ; then
         chown -R $NB_UID /home/$NB_USER/$repo /opt/conda/envs/python2/lib/python2.7/site-packages/*.egg-link /opt/conda/envs/python2/lib/python2.7/site-packages/*.pth /opt/conda/pkgs/cache
       fi
